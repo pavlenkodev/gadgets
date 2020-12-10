5b371d24fa2b88edd0b7ae99a1233b9e1464f49b
Dmitry <pavlenkod.v.2@gmail.com>
2020-12-10 23:46:09 +0300

    update pom

--- src/main/java/tech/itpark/marketplace/repository/DescriptionRepository.java
+++ src/main/java/tech/itpark/marketplace/repository/DescriptionRepository.java
@@ -3,7 +3,7 @@ package tech.itpark.marketplace.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import tech.itpark.marketplace.model.Description;
-@EnableJpaRepositories
+
 public interface DescriptionRepository extends JpaRepository<Description, Long> {
 
 }
